7fbf666c8d4b01ed911a34a819d4ad12034daebe
VigoTheHacker <vigostudio77@gmail.com>
2021-05-16 10:01:34 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vigothehacker/React3d
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	quoteV3 "rsc.io/quote/v3"
+)
+
+func Hello() string {
+	return quoteV3.HelloV3()
+}
+
+func Proverb() string {
+	return quoteV3.Concurrency()
+}
